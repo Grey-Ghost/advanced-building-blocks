609cc5778ac34b4d32fa7227ca43011c2c7b1305
Grey_Ghost <kevin.rabe@webmail.co.za>
2017-04-22 22:47:03 +0200

    initial commit

---
+++ bubble_sort.rb
@@ -0,0 +1,38 @@
+
+def bubble_sort to_sort
+  loop do
+    swapped = false
+    for i in 1...to_sort.length do
+      if to_sort[i-1] > to_sort[i]
+        swap = to_sort[i-1]
+        to_sort[i-1] = to_sort[i]
+        to_sort[i] = swap
+        swapped = true
+      end
+    end
+    break if swapped == false
+   end
+   return to_sort
+end
+
+def bubble_sort_by to_sort
+  loop do
+    swapped = false
+    for i in 1...to_sort.length do
+      if yield(to_sort[i], to_sort[i-1]) < 0
+        swap = to_sort[i-1]
+        to_sort[i-1] = to_sort[i]
+        to_sort[i] = swap
+        swapped = true
+      end
+    end
+    break if swapped == false
+  end
+  return to_sort
+end
+
+puts "#{bubble_sort([4,3,78,2,0,2])}"
+
+puts "#{print(bubble_sort_by(["hi","hello","hey"]) do |left, right|
+    left.length - right.length
+end)}"
